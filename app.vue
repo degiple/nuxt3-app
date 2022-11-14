d5a73efc99c69e5cdb2fcbd0d9941411ee573dee
degiple <k.takeshima@degiple.com>
2022-11-14 12:03:59 +0000

    Applied pug

--- app.vue
+++ app.vue
@@ -1,10 +1,7 @@
-<template>
-  <v-app>
-    <v-app-bar app>
-      <v-app-bar-title>Nuxt3 & Vuetify3 & Volar project</v-app-bar-title>
-    </v-app-bar>
-    <v-main>
-      <nuxt-page></nuxt-page>
-    </v-main>
-  </v-app>
+<template lang="pug">
+v-app
+  v-app-bar(app)
+    v-app-bar-title Nuxt3 & Vuetify3 & Volar project
+  v-main
+    nuxt-page 
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
-<template>
-  <v-container>
-    <h1>This is Nuxt3 & Vuetify3 & Volar project!</h1>
-    <v-btn @click="ready = !ready">ready?</v-btn>
-    <div class="py-3">ready: {{ ready }}</div>
-  </v-container>
+<template lang="pug">
+v-container
+  h1 This is Nuxt3 & Vuetify3 & Volar project!
+  v-btn(@click="ready = !ready") ready?
+  div(class="py-3") ready: {{ ready }}
 </template>
 
 <script setup lang="ts">
