35a32c29c9b32b5f2a99f8f6456750f408804c7b
Konosuke Takeshima <k.takeshima@degiple.com>
2022-11-14 13:11:01 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-app-bar-title>Nuxt3 & Vuetify3 & Volar project</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <nuxt-page></nuxt-page>
+    </v-main>
+  </v-app>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <h1>This is Nuxt3 & Vuetify3 & Volar project!</h1>
+    <v-btn @click="ready = !ready">ready?</v-btn>
+    <div class="py-3">ready: {{ ready }}</div>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+const ready = ref(false);
+</script>
